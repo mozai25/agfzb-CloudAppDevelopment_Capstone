71615af4a2cbe42f6520b46bf477f57ea03e7d71
mozai25 <prolactin@gmail.com>
2024-03-11 14:18:20 -0400

    update

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -8,12 +8,12 @@ admin.site.register(CarModel)
 admin.site.register(CarMake)
 
 # CarModelInline class
-#class CarModelInline(admin.StackedInline):
-#    model = CarModel 
-#    extra = 5
+class CarModelInline(admin.StackedInline):
+    model = CarModel 
+    extra = 5
 
-#class CarMakeInline (CarMake):
-#    inlines = [CarModelInline]
+class CarMakeInline (CarMake):
+    inlines = [CarModelInline]
 
 # CarModelAdmin class
 
