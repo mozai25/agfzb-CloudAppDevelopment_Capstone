7d1e8e431fb385cd2fac058ef34cec573aff4125
mozai25 <prolactin@gmail.com>
2024-03-11 14:01:32 -0400

    update

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel, CarDealer, DealerReview  
 
 
 # Register your models here.
 
+admin.site.register(CarModel)
+admin.site.register(CarMake)
+
 # CarModelInline class
+#class CarModelInline(admin.StackedInline):
+#    model = CarModel 
+#    extra = 5
+
+#class CarMakeInline (CarMake):
+#    inlines = [CarModelInline]
 
 # CarModelAdmin class
 
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -13,7 +13,8 @@ from django.utils.timezone import now
 class CarMake(models.Model):
     name = models.CharField(null=False, max_length=50, default='BMW')
     description = models.CharField(null=False, max_length=250, default='BMW - luxury car')
-    power = models.CharField(null=False, max_length=10, default='300hp')
+    
+#   power = models.CharField(null=False, max_length=10, default='300hp')
 
     def __str__(self):
         return self.name + " " + self.description
@@ -29,11 +30,48 @@ class CarMake(models.Model):
 
 class CarModel(models.Model):
     name = models.CharField(null=False, max_length=50, default='BMW 525')
-    car_type = models.CharField(null=False, max_length=50, default='Sedan')
     year = models.DateField(null=False, max_length=50, default='2022')
+    make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+    dealerId = models.IntegerField()
+    SEDAN = 'audit'
+    SUV = 'honor'
+    WAGON = 'wagon'
+    CARS_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'Suv'),
+        (WAGON, 'Wagon'),
+    ]
+    car_type = models.CharField(null=False, choices=CARS_TYPES, max_length=50, default='Sedan')
 
+    def __str__(self):
+        return self.name + " " + self.year + " " + self.make + " " + self.dealerId + " " + self.car_type
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
+class CarDealer(): 
+    
+    def __init__(self, id, city, state, st, address, zip, lat, long,short_name, full_name):
+       self.id = id
+       self.city = city
+       self.state = state
+       self.st = st
+       self.address = address
+       self.zip = zip
+       self.lat = lat
+       self.long = long
+       self.short_name = short_name
+       self.full_name = full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview(): 
+    
+    def __init__(self, id, name, dealership, review, purchase, purchase_date, car_make, car_model, car_year):
+        self.id = id
+        self.name = name
+        self.dealer = dealer
+        self.review = review
+        self.purchase = purchase
+        self.purchase_date = purchase_date
+        self.car_make = car_make
+        self.car_model = car_model
+        self.car_year = car_year
+        
